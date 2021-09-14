38c15dcf548bd9ff4b57c5f1b6a0a8b125d45585
KianYang-Lee <ken.kianyang.lee@gmail.com>
2021-09-14 17:01:38 +0800

    add notes for chap 1

--- src/main/java/com/example/chap1/Dollar.java
+++ src/main/java/com/example/chap1/Dollar.java
@@ -1,6 +1,16 @@
 package com.example.chap1;
 
-//// ======================================= initial state at beginning of chapter
+// ================================== NOTES =========================================== //
+/*
+ * Cycle of TDD :
+ * 1. Add a little test
+ * 2. Run all tests and fail.
+ * 3. Make a little changes.
+ * 4. Run the tests and succeed.
+ * 5. Refactor to remove duplication.
+ */
+
+//// ======================================= initial state at beginning of chapter =========================== //
 //public class Dollar {
 //    public Dollar(int amount) {
 //
@@ -10,7 +20,7 @@ package com.example.chap1;
 //    int amount;
 //}
 
-// ======================================== final state at end of chapter
+// ======================================== final state at end of chapter =================================== //
 public class Dollar {
 
     // instance variable
